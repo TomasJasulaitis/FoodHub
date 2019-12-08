57df7298777ca8db575c44d3d3938de7aaa635ca
Tomas Jasulaitis <tomis.jasulaitis@gmail.com>
2019-12-08 14:42:19 +0200

    AuthController

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginFormRequest;
+use App\Http\Requests\RegistrationFormRequest;
+use App\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+
+    public function register(RegistrationFormRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $token = auth()->login($user);
+
+        return $this->respondWithToken($token);
+    }
+
+    public function login(LoginFormRequest $request)
+    {
+        $credentials = $request->only(['email', 'password']);
+        $token = auth()->attempt($credentials);
+
+        if (!$token) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid Email or Password',
+            ], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+    public function getAuthUser(Request $request)
+    {
+        return response()->json(auth()->user());
+    }
+    public function logout(Request $request)
+    {
+        $this->validate($request, [
+            'token' => 'required'
+        ]);
+
+        auth()->logout();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'User logged out successfully',
+        ]);
+    }
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
+}
